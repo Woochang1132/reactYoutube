f712c565e763605b71ab6d429983c6e22a440a1e
Woochang1132 <show4031132@gmail.com>
2023-10-26 14:44:59 +0900

    최우창 react_youtube 챌린지, router 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import { Outlet } from "react-router-dom/dist";
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Outlet></Outlet>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom/dist";
+import NotFound from "./pages/NotFound";
+import Videos from "./pages/Videos";
+import VideoDetail from "./pages/VideoDetail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: "videos", element: <Videos /> },
+      // keyword가 없는 경우는 popular 목록을
+      // keyword에 맞는 비디오 목록을 보여준다.
+      { path: "videos/:keyword", element: <Videos /> },
+      { path: "videos/watch/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
