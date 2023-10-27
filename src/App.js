fc155c5cd57e98467257623346adec7806237e68
Woochang1132 <show4031132@gmail.com>
2023-10-27 15:38:09 +0900

    최우창 react_youtube 챌린지 api에서 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom/dist";
+import SearchHeader from "./components/SearchHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 export default function App() {
   return (
     <>
-      <Outlet></Outlet>
+      <SearchHeader />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,9 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { useParams } from 'react-router-dom/dist';
+import VideoCard from '../components/VideoCard';
 
 export default function Videos() {
+    // 객체에 어떤 keyword가 있는 지 알 수 있다.
+    const {keyword} = useParams();
+    const {isLoding, error, data:videos} =useQuery({
+        queryKey :['videos', keyword] , 
+        queryFn: async () => {return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+        .then((res) => res.json())
+        .then((data) => data.items)
+    }});
     return (
         <div>
-            Videos
+            Videos {keyword ? `🖱${keyword}` : '🔥'}
+            {isLoding && <p>Loding...</p>}
+            {error && <p>Something is wrong</p>}
+            {videos && 
+                <ul>
+                {videos.map(video => <VideoCard key={video.id} video={video}/>)}
+                </ul>}
         </div>
     );
 }
