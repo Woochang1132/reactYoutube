3eb5d09571a3ff4e8672b2f09f3403c3ed37c42f
Woochang1132 <show4031132@gmail.com>
2023-10-27 18:17:15 +0900

    최우창 react_ 네트워크 통신, axios 활용하기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,15 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom/dist';
 import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
     // 객체에 어떤 keyword가 있는 지 알 수 있다.
     const {keyword} = useParams();
     const {isLoding, error, data:videos} =useQuery({
         queryKey :['videos', keyword] , 
-        queryFn: async () => {return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-        .then((res) => res.json())
-        .then((data) => data.items)
+        queryFn: async () => {return axios.get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+        .then((res) => res.data.items);
     }});
     return (
         <div>
