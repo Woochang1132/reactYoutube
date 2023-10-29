084adf5ef7015d00aab1e542f6084bb2a93f83b2
Woochang1132 <show4031132@gmail.com>
2023-10-29 23:54:55 +0900

    최우창 react youtube api, 검색 및 인기 동영상 연결

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,13 +1,43 @@
 import axios from "axios";
 
-export async function search(keyword) {
-  return axios
-    .get(`/videos/${keyword ? "search" : "popular"}.json`)
-    .then((res) => res.data.items)
-    .catch((error) => {
-      console.log("error", error);
-    })
-    .finally(() => {
-      console.log("네트워크 통신 확인");
+export default class Youtube {
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: "https://www.googleapis.com/youtube/v3",
+      params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
     });
+  }
+
+  async search(keyword) {
+    return keyword ? this.#searchBykeyword(keyword) : this.#mostPopular();
+  }
+
+  async #searchBykeyword(keyword) {
+    return this.httpsClient
+      .get(`search`, {
+        params: {
+          part: "snippet",
+        },
+      })
+      .then((res) => res.data.items)
+      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })))
+      .catch((error) => {
+        console.log("error", error);
+      })
+      .finally(() => {
+        console.log("네트워크 통신 확인");
+      });
+  }
+
+  async #mostPopular() {
+    return axios
+      .get(`/videos/popular.json`)
+      .then((res) => res.data.items)
+      .catch((error) => {
+        console.log("error", error);
+      })
+      .finally(() => {
+        console.log("네트워크 통신 확인");
+      });
+  }
 }
