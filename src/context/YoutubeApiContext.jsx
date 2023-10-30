03d3e062ef60566a64d577e476f68bbda5fbc572
Woochang1132 <show4031132@gmail.com>
2023-10-30 17:16:26 +0900

    최우창 react_youtube 챌린지 videoCard 구성하기 진행중

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -2,11 +2,12 @@ import { createContext, useContext } from 'react';
 import Youtube from '../api/youtube';
 import FakeYoutube from '../api/fakeYoutubeClient';
 import YoutubeClient from '../api/youtubeClient';
+import FakeYoutubeClient from '../api/fakeYoutubeClient';
 
 export const YoutubeApiContext = createContext();
 
 //const youtube = new FakeYoutube();
-const client = new YoutubeClient();
+const client = new FakeYoutubeClient();
 const youtube = new Youtube(client);
 export function YoutubeApiProvider({children}){
     return <YoutubeApiContext.Provider value={{youtube}}>
